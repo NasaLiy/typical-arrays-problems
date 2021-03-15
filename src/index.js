7bb1220402237fc30ec0b816d6f136ca52ede0f3
Nataliy <nataliy.leonidovna@gmail.com>
2021-03-15 16:53:26 +0200

    Add functions max, min, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 
 exports.min = function min (array) {
-  return 0;
+    return (array && array.length) ? Math.min(...array) : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+    return (array && array.length) ? Math.max(...array) : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    let sum;
+    if(array && array.length){
+        sum = array.reduce((a, b) => a + b);
+    }
+    return (array && array.length) ? sum/array.length : 0;
 }
